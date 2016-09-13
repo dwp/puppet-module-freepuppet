2c491afa22d9e0cd39b900359b70d4e871d8cb5d
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-13 13:51:34 +0100

    Minor changes to update install structure.

--- files/install/ubuntu_1404.sh
+++ files/install/ubuntu_1404.sh
@@ -5,7 +5,10 @@ MANIFEST_NAME_FILE=/etc/puppet/manifest
 NODE_NAME_FILE=/etc/puppet/certname
 
 # setup required modules
-apt-get install puppet-common git
+apt-get install puppet-common git rubygems-integration
+
+# install librarian pupet
+gem install librarian-puppet
 
 # setup manifest file to run
 echo "Please enter the name of the manifest file you wish to run:"
@@ -22,6 +25,12 @@ if [ ! -f NODE_NAME_FILE ]
 then 
     echo -n "${NODE_NAME}" > "${NODE_NAME_FILE}"
 fi
+
+# perform a librarian-puppet install
+$(
+  cd /etc/puppet
+  /usr/local/bin/librarian-puppet install
+)
 
 # now perform our first puppet run
 /usr/bin/puppet apply --certname $NODE_NAME --modulepath /etc/puppet/modules /etc/puppet/manifests/${MANIFEST_NAME}.pp
