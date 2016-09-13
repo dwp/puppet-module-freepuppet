5af6de97a6427f3a90450d6dc11eaf9cebae8d20
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-13 14:02:55 +0100

    Whitespace issues.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,18 @@
 # @author jonty.bale@dwp.gsi.gov.uk
 #
 class freepuppet (
-	$ensure = 'present'
+    $ensure = 'present'
 ) {	
-	# ensure the run script is in place
-	file { "/usr/local/bin/freepuppet-run":
-		ensure => $ensure,
-		source => "puppet:///modules/freepuppet/cron/run.sh"
-	}
-	
-	# ensure we have a cron in place which runs every 15min
-	cron { "freepuppet-run":
-		ensure => $ensure,
-		command => "/usr/local/bin/freepuppet-run",
-		minute  => '*/15'
-	}
+    # ensure the run script is in place
+    file { "/usr/local/bin/freepuppet-run":
+        ensure => $ensure,
+        source => "puppet:///modules/freepuppet/cron/run.sh"
+    }
+
+    # ensure we have a cron in place which runs every 15min
+    cron { "freepuppet-run":
+        ensure => $ensure,
+        command => "/usr/local/bin/freepuppet-run",
+        minute  => '*/15'
+    }
 }
