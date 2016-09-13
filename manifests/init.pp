2811f24f2a15edd4a9478f248b137c2ebaaf1c88
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-13 14:51:52 +0100

    Fixing formatting issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,17 +3,17 @@
 # @author jonty.bale@dwp.gsi.gov.uk
 #
 class freepuppet
-{	
+{
     # ensure the run script is in place
-    file { "/usr/local/bin/freepuppet-run":
-        ensure => $ensure,
-        source => "puppet:///modules/freepuppet/cron/run.sh"
+    file { '/usr/local/bin/freepuppet-run':
+        ensure => present,
+        source => 'puppet:///modules/freepuppet/cron/run.sh'
     }
 
     # ensure we have a cron in place which runs every 15min
-    cron { "freepuppet-run":
-        ensure => $ensure,
-        command => "/usr/local/bin/freepuppet-run",
+    cron { 'freepuppet-run':
+        ensure  => present,
+        command => '/usr/local/bin/freepuppet-run',
         minute  => '*/15'
     }
 }
