a6d26e68cd3c6b994dc88333d22699d2854ec2b6
Jonty Bale <jonty.bale@dwp.gsi.gov.uk>
2016-09-29 11:55:04 +0100

    Updating licence details.

--- files/cron/update.sh
+++ files/cron/update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# this code will wrap the run script and provide a process to update the puppet repository from a central location.
+# this code will wrap the run script and provide a process to update the puppet repository from
+# a central location once the code is on github.
